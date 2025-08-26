62e4b03f1a1f81c69d1ffd814ce59e3691a158c2
Leonardo <werlkd@gmail.com>
2025-08-25 18:20:18 -0600

    added files and poo document video 14

---
+++ PrimerosPasos/src/EntradaDatos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EntradaDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in); // Representa entrada por teclado.
+		
+		System.out.println("Introduce tu nombre, porfavor");
+		
+		String nombre_usuario = entrada.nextLine(); // Básicamente hace la lectura de lo que se escribio en consola 
+													//y lo guarda en una variable.
+		System.out.println("Introduce tu edad, porfavor");
+		
+		int edad= entrada.nextInt(); // guarda un objeto de tipo entero por consola.
+		
+		System.out.println("Hola " + nombre_usuario + " Tienes " + edad + " años"); //el programa entra en modo de ejecución.
+		
+		entrada.close(); // Una vez que terminas de usar la via de comunicación, es recomendable cerrar para no consumir recursos.
+		
+		
+	}
+
+}
