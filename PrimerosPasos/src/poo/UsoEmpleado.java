491288a475ce51a8fd8b6af8ef254b4e4625c721
Leonardo <werlkd@gmail.com>
2025-09-11 17:24:24 -0600

    feat(vid33-construct-param): added file update poo and create usoEmpleado

---
+++ PrimerosPasos/src/poo/UsoEmpleado.java
@@ -0,0 +1,58 @@
+package poo;
+
+import java.util.Date; // Se importa de la api de java, la clase Date de java.util
+
+
+
+public class UsoEmpleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Las clases se generan en otra carpeta dependiendo del paquete
+		 * para mejor organización.
+		 * 
+		 * 
+		 * Puedes crear una aplicación en donde los archivos java esten separados.
+		 * 
+		 * También puedes crear un archivo java en donde se tienen 2 o más clases.
+		 * 
+		 * 
+		 * Para efectos de practicidad es mas comodo porque generas menos archivos.
+		 * 
+		 * solo para aplicaciones pequeñas.
+		 * 
+		 * Mala práctica para proyectos medianos o grandes.
+		 * 
+		 * Solo cuando se meten clases en un mismo archivo, solo la que contiene el método main
+		 * puede ser clase public, cuando hay clases separadas, pueden haber más clases públic.
+		 * 
+		 * 
+		 * */
+		
+		
+		
+	}
+
+}
+
+class Empleado {
+	
+	public Empleado(String nom, double sue, int agno, int mes, int dia) {
+		/* Se crea el estado incial de este empleado va a tener todos los parametros declarados,
+		 * en el constructor de la clase Empleado.
+		 * 
+		 * 
+		 * */
+		
+	}
+	
+	private String nombre;
+	
+	private double sueldo;
+	
+	private Date altaContrato; // Mandan advertencia porque aun no se utilizan las variables.
+	
+	
+	
+}
